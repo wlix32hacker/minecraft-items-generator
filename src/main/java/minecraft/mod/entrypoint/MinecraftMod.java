232ce7172ad08b92b4501eac8e6ab145b6571b10
unknown <wlix32hacker@gmail.com>
2020-06-21 23:51:37 -0300

    setup form

--- src/main/java/minecraft/mod/entrypoint/MinecraftMod.java
+++ src/main/java/minecraft/mod/entrypoint/MinecraftMod.java
@@ -1,5 +1,6 @@
 package minecraft.mod.entrypoint;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,13 +10,14 @@ public class MinecraftMod {
 
   private JPanel panel;
   private JComboBox minecraftVersion;
-  private JTextField textField1;
-  private JComboBox comboBox2;
-  private JComboBox comboBox1;
-  private JTextField textField2;
+  private JTextField targetQtdIpt;
+  private JComboBox targetItemTypeSlc;
+  private JComboBox sourceItemTypeSlc;
+  private JTextField sourceQtdIpt;
+  private JButton findAndChangeButton;
 
   public static void main(String[] args) {
-    JFrame frame = new JFrame("MinecraftMod");
+    JFrame frame = new JFrame("Minecraft Mod v1.0");
     frame.setContentPane(new MinecraftMod().panel);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.pack();
